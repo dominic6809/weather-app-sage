092f85a866f2f3a5b2e682cde4202931ee54649b
Siren2278 <muuomusyoki2278@gmail.com>
2025-04-20 14:14:59 +0300

    updated my test suites

--- weather-api/tests/Feature/ExampleTest.php
+++ weather-api/tests/Feature/ExampleTest.php
@@ -3,17 +3,14 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class WeatherApiTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_api_returns_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->getJson('/api/geocoding'); 
         $response->assertStatus(200);
     }
 }
